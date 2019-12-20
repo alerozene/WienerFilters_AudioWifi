939bacd2d07e5b2bd790eda423d55d8d8dc0eda0
alejandrorv68 <31384598+alejandrorv68@users.noreply.github.com>
2019-12-20 20:07:15 +0100

    Update testfunctionScript.py

--- python/testfunctionScript.py
+++ python/testfunctionScript.py
@@ -13,8 +13,18 @@ serialPort = serial.Serial(port= 'COM11', '9600', timeout=0, rtscts=True)
 serialPort.inWaiting()
 x = serialPort.read().decode("utf-8")
 print(x)
+
+# List serial ports, in the description, arduino mkr1000 is indicated
+from serial.tools import list_ports
+x = list(list_ports.comports())
+print(x)
+
+
+
 """
 
 serialPort = serial.Serial('COM11', 9600, timeout=0, rtscts=True)
 serialPort.write(b'Send stuff\n')
 serialPort.close
+
+
