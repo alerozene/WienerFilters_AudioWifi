ae81273781b1e6d9c2c5c0706d954c7de20d69e0
alejandrorv68 <31384598+alejandrorv68@users.noreply.github.com>
2019-12-30 14:38:40 +0100

    This version can send (repeatedly) float arrays

--- python/testfunctionScript.py
+++ python/testfunctionScript.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 18 15:37:28 2019
-Read simple serial data
+Load data via serial to micro-controller board. 
 
 @author: arozenevallesp
 """
+
 import serial
-#import numpy
+import numpy
+
 
 """
 #Of course, this works for reading serial data
@@ -14,52 +16,46 @@ serialPort = serial.Serial(port= 'COM11', '9600', timeout=0, rtscts=True)
 serialPort.inWaiting()
 x = serialPort.read().decode("utf-8")
 print(x)
-
 # List serial ports, in the description, arduino mkr1000 is indicated
 from serial.tools import list_ports
 x = list(list_ports.comports())
 print(x)
-
-
-
 """
 
 
+#%%
 try:
     serialPort = serial.Serial('COM15', 9600, timeout=0, rtscts=True)
     
 except serial.serialutil.SerialException:
     serialPort.close()
     serialPort.open()
-
-serialPort.write(b'WRITE\n')
-serialPort.close
-
-"""
-
-
-serialPort = serial.Serial('COM15', 9600, timeout=0, rtscts=True)
-serialPort.open
-serialPort.write(b'WRITE\n')
-serialPort.close
-
-
+#%%
 
 # seed 
 numpy.random.seed(1)
 # Array with random numbers
-values = numpy.random.rand(10)
+values = numpy.random.rand(1000)
 values = numpy.around(values, decimals=2)
-values = values.astype('|S4')
+values = values.astype('|S5')
+values = [x+b'\n' for x in values]
+#%%
 
+try:
+    serialPort.close()    
+except serial.serialutil.SerialException:
+    serialPort.open()
+    serialPort.close()
+#%%
+for x in range(len(values)): 
+    serialPort.open()
+    serialPort.write(values[x])
+    serialPort.close()
+
+#%%
 
 
 for x in numpy.nditer(values):
     print(x)
-    serialPort.write(x)
-    serialPort.write(b'\n')
-serialPort.close
-
-"""
 
 
